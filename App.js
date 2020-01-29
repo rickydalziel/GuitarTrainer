4b4eb08a3373c3559649f4b22183aa172091ac16
Ricky Dalziel <ricky@floatapp.com>
2020-01-29 17:01:43 +0000

    Write first test and fix linter

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
 import React, { Component } from 'react'
-import { Platform, StyleSheet, Text, View } from 'react-native'
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu'
-})
+import { StyleSheet, Button, Text, View } from 'react-native'
 
 export default class App extends Component {
+  state = { clickCount: 0 }
+
   render () {
+    const { clickCount } = this.state
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Text style={styles.title}>Guitar Trainer</Text>
+        <Text style={styles.instructions}>Please select a training option below</Text>
+        <Text style={styles.instructions}>Button has been pressed {clickCount} times</Text>
+        <Button title='Note Teacher' onPress={() => this.setState({ clickCount: clickCount + 1 })} />
       </View>
     )
   }
@@ -27,7 +24,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF'
   },
-  welcome: {
+  title: {
     fontSize: 20,
     textAlign: 'center',
     margin: 10
@@ -35,6 +32,6 @@ const styles = StyleSheet.create({
   instructions: {
     textAlign: 'center',
     color: '#333333',
-    marginBottom: 5
+    marginBottom: 10
   }
 })
--- __tests__/App.js
+++ __tests__/App.js
@@ -1,4 +1,4 @@
-import 'react-native'
+import ReactNative from 'react-native'
 import React from 'react'
 import App from '../App'
 
@@ -6,5 +6,15 @@ import App from '../App'
 import renderer from 'react-test-renderer'
 
 it('renders correctly', () => {
-  renderer.create(<App />)
+  expect(renderer.create(<App />)).toMatchSnapshot()
+})
+
+it('increments a counter when the note teacher button is pressed', () => {
+  const app = renderer.create(<App />).root
+  const button = app.findByType(ReactNative.Button)
+
+  button.props.onPress()
+
+  const counter = app.findAllByType(ReactNative.Text)[2]
+  expect(counter.props.children[1]).toBe(1)
 })
